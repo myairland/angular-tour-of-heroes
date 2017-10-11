854f23636c7e66a2c0870e4c1068e53f9d576173
myairland <myairland@gmail.com>
2017-10-12 01:13:03 +0800

    http module code finish

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -37,5 +37,17 @@ export class HeroesComponent {
   gotoDetail():void{
     this.router.navigate(['/detail',this.selectedHero.id]); 
   }
+  
+  add(name:String):void{
+    name = name.trim();
+    if(!name)
+      return;
+    
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      })
+  }
 
 }
